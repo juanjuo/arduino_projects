f96d775f8b726c7f448edab499ee5476afcc0490
Juan Diego Mora Rubio <juanjuo37@gmail.com>
2025-11-01 10:16:37 -0400

    added some useful functions that might help get started

--- midi_maracas_controller/main/main.ino
+++ midi_maracas_controller/main/main.ino
@@ -1,3 +1,23 @@
+//Juan Diego Mora
+//10/30/2025
+//Wireless Maraca Midi Controller
+
+/*
+some methods from CodeCell
+
+Motion_AccelerometerRead(float &x, float &y, float &z) // Accelerometer (m/s²)
+Motion_GyroRead(float &x, float &y, float &z) // Gyroscope (°/s)
+Motion_MagnetometerRead(float &x, float &y, float &z) // Magnetometer (µT)
+Motion_GravityRead(float &x, float &y, float &z) // Gravity vector
+Motion_LinearAccRead(float &x, float &y, float &z) // Linear acceleration
+Motion_RotationRead(float &roll, float &pitch, float &yaw) // Rotation (degrees)
+Motion_RotationNoMagRead(float &roll, float &pitch, float &yaw) // Rotation w/o mag
+Motion_RotationVectorRead(float &vec_r, float &vec_i, float &vec_j, float &vec_k) // Quaternion
+
+myCodeCell.PrintSensors(); //to print all enabled sensors to serial monitor
+
+*/
+
 #include <CodeCell.h>
 #include <BLEMIDI_Transport.h>
 #include <hardware/BLEMIDI_ESP32.h>
