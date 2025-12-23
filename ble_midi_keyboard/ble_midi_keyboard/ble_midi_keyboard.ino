82f10bf804f84326f5013619c5ac47b427f34977
Juan Diego Mora <juanjuo37@gmail.com>
2025-12-23 14:22:11 -0500

    working on it

--- ble_midi_keyboard/ble_midi_keyboard/ble_midi_keyboard.ino
+++ ble_midi_keyboard/ble_midi_keyboard/ble_midi_keyboard.ino
@@ -5,11 +5,11 @@ BLEMIDI_CREATE_INSTANCE("BLE_MIDI_KEYBOARD", MIDI)
 
 const int NUM_KEYS = 12;
 
-int keyPins[NUM_KEYS] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20 };
+int keyPins[NUM_KEYS] = { 0, 1, 2, 3, 4, 5, 6, 7, 10, 21};
 
-int keyLastState[NUM_KEYS] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int keyLastState[NUM_KEYS] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-int notes[NUM_KEYS] = { 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71 };
+int notes[NUM_KEYS] = { 60, 61, 62, 63, 64, 65, 66, 67, 68, 70};
 
 int keyState = LOW;
 
@@ -17,16 +17,16 @@ bool isConnected = false;
 
 
 void setup() {
-  Serial.begin(115200);
+  //Serial.begin(115200);
 
   BLEMIDI.setHandleConnected([]() {
     isConnected = true;
-    Serial.println("Connected");
+    //Serial.println("Connected");
   });
 
   BLEMIDI.setHandleDisconnected([]() {
     isConnected = false;
-    Serial.println("Disconnected");
+    //Serial.println("Disconnected");
   });
 
   for (int i = 0; i < NUM_KEYS; i++) {
@@ -43,12 +43,12 @@ void loop() {
       keyState = digitalRead(keyPins[i]);
 
       if (keyState == LOW && keyLastState[i] == 0) {
-        Serial.println("Button Pressed");
+        //Serial.println("Button Pressed");
         MIDI.sendNoteOn(notes[i], 127, 1);
         keyLastState[i] = 1;
       } else if (keyState == HIGH && keyLastState[i] == 1) {
         MIDI.sendNoteOff(notes[i], 127, 1);
-        Serial.println("RESET");
+        //Serial.println("RESET");
         keyLastState[i] = 0;
       }
     }
