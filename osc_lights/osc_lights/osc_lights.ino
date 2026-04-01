f507f4b0fc94a7b1d465c977fbe273002ac90b0f
Juan Diego Mora <juanjuo37@gmail.com>
2026-04-01 11:07:29 -0400

    osc working

--- osc_lights/osc_lights/osc_lights.ino
+++ osc_lights/osc_lights/osc_lights.ino
@@ -1,66 +1,107 @@
-// #include <ArduinoOSCWiFi.h>
-
-// // WiFi stuff
-// const char* ssid = "your-ssid";
-// const char* pwd = "your-password";
-// const IPAddress ip(192, 168, 1, 201);
-// const IPAddress gateway(192, 168, 1, 1);
-// const IPAddress subnet(255, 255, 255, 0);
-
-// // for ArduinoOSC
-// const char* host = "192.168.1.200";
-// const int recv_port = 54321;
-// const int bind_port = 54345;
-// const int send_port = 55555;
-// const int publish_port = 54445;
-
-// int i;
-// float f;
-// String s;
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
 
 const int ledPin = 13;
-const int pwmFreq = 1000;     // 1 kHz
-const int pwmResolution = 8;  // 8-bit PWM (0–255)
+// const int pwmFreq = 1000;     // 1 kHz
+// const int pwmResolution = 8;  // 8-bit PWM (0–255)
 
-// void onOscReceived(const OscMessage& m) {
-//     Serial.print(m.remoteIP());
-//     Serial.print(" ");
-//     Serial.print(m.remotePort());
-//     Serial.print(" ");
-//     Serial.print(m.size());
-//     Serial.print(" ");
-//     Serial.print(m.address());
-//     Serial.print(" ");
-//     Serial.print(m.arg<int>(0));
-//     Serial.print(" ");
-//     Serial.print(m.arg<float>(1));
-//     Serial.print(" ");
-//     Serial.print(m.arg<String>(2));
-//     Serial.println();
-// }
+char ssid[] = "Joan and Jeff (appt)";
+char password[] = "easycanoe613";
+
+WiFiUDP udp;
+const int localPort = 57120;
 
 void setup() {
-  // delay(2000);
+  Serial.begin(115200);
 
-  ledcAttach(ledPin, pwmFreq, pwmResolution);
+  //Connect to WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected!");
+  Serial.print("ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+
+  // Start UDP listener
+  udp.begin(localPort);
+  Serial.printf("Listening for OSC on port %d\n", localPort);
+
+  // LED CONTROL
+  //ledcAttach(ledPin, pwmFreq, pwmResolution);
+
+  pinMode(ledPin, OUTPUT);
+
   Serial.begin(115200);
 }
 
 void loop() {
+  OSCMessage msg;
+  int packetSize = udp.parsePacket();
 
-  for (int value = 0; value <= 255; value++) {
-    ledcWrite(ledPin, value);
-    Serial.println(value);
-    delay(2);
+  if (packetSize) {
+    // Fill OSC message with incoming bytes
+    while (packetSize--) {
+      msg.fill(udp.read());
+    }
+
+    if (!msg.hasError()) {
+      printOSCMessage(msg);
+    } else {
+      Serial.println("OSC message error");
+    }
   }
 
-  for (int value = 255; value >= 0; value--) {
-    ledcWrite(ledPin, value);
-    Serial.println(value);
-    delay(2);
+  if (msg.isInt(0)) {
+    analogWrite(ledPin, msg.getInt(0));
   }
 
+
+  // for (int value = 0; value <= 255; value++) {
+  //   ledcWrite(ledPin, value);
+  //   Serial.println(value);
+  //   delay(2);
+  // }
+
+  // for (int value = 255; value >= 0; value--) {
+  //   ledcWrite(ledPin, value);
+  //   Serial.println(value);
+  //   delay(2);
+  // }
 }
 
+void printOSCMessage(OSCMessage& msg) {
+  // Print address
+  char address[64];
+  msg.getAddress(address);
+  Serial.print("Address: ");
+  Serial.println(address);
 
+  // Print arguments
+  Serial.print("Args: ");
 
+  for (int i = 0; i < msg.size(); i++) {
+    if (msg.isInt(i)) {
+      Serial.print(msg.getInt(i));
+    } else if (msg.isFloat(i)) {
+      Serial.print(msg.getFloat(i));
+    } else if (msg.isString(i)) {
+      char str[64];
+      msg.getString(i, str, 64);
+      Serial.print(str);
+    } else {
+      Serial.print("[unknown]");
+    }
+
+    if (i < msg.size() - 1) {
+      Serial.print(", ");
+    }
+  }
+
+  Serial.println();
+  Serial.println("--------------------");
+}
